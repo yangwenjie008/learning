3997dd54644d9073b6b1b547962e74aba60f7ff8
aaron.yang <wenjie1051031930@163.com>
2021-01-26 01:53:00 +0800

    add didispace module

---
+++ spring-cloud-learning/didispace/src/main/java/com/aaron/didispace/DidispaceApplication.java
@@ -0,0 +1,13 @@
+package com.aaron.didispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidispaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidispaceApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-learning/didispace/src/test/java/com/aaron/didispace/DidispaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron.didispace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidispaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
